2e8c9f18331d1516797fe207ed173654281eecd5
Sudesh7022 <sudesh7022@gmail.com>
2026-03-28 07:13:45 +0000

    added nums and maths file

---
+++ 01_basics/Dates_time.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("03-28-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// '${newDate.getDay()} and the time'
+
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+    timeZone: "Asia/Kolkata"
+})
